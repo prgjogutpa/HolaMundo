2350826cbbeee75994065b902354f46d72924c06
prgjogutpa <prgjogutpa@gmail.com>
2023-09-27 18:51:19 +0200

    Incorporación punto de entrada

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -28,7 +28,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo8");
+        String nombre = "jogutpa";
+        System.out.println("Hola " + nombre);
     }
     
 }
