8875bb1bd5155e535b3829556aa7f5bd0635ffb8
prgjogutpa <prgjogutpa@gmail.com>
2023-09-27 18:56:30 +0200

    concadenación de variables :sparkles:

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -28,7 +28,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String nombre = "jogutpa";
+        String nombre = "jogutpa xd";
         System.out.println("Hola " + nombre);
     }
     
